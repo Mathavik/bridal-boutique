a628f415cc93cf367b6625c180d6c6b10007cbd6
mahalakshmi-v508 <mahalakshmivelu508@gmail.com>
2026-06-30 12:49:52 +0530

    wholesale

--- Bridal-Boutique-Frontend/src/App.jsx
+++ Bridal-Boutique-Frontend/src/App.jsx
@@ -1,14 +1,26 @@
-
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header"; // Correct path
+
+function Home() {
+  return (
+    <>
+      <Header />
+
+      <div className="p-10 text-center">
+        <h1 className="text-4xl font-bold">Bridal Boutique</h1>
+        <p className="mt-4 text-gray-500">
+          Welcome to our Bridal Boutique
+        </p>
+      </div>
+    </>
+  );
+}
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-          {/* 🔓 Public */}
-          <Route path="/" element={<div>This is app</div>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
