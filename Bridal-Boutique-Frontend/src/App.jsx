05ac37ba0d1d11b632e8f7459e2defe61a139c25
jeya-jesina <jeyajesina@gmail.com>
2026-06-30 12:34:17 +0530

    create files

--- Bridal-Boutique-Frontend/src/App.jsx
+++ Bridal-Boutique-Frontend/src/App.jsx
@@ -1,7 +1,35 @@
-import AppRoutes from "./routes/AppRoutes";
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import "./App.css";
+
+import Home from "./Home/Home";
+
 function App() {
-  return <AppRoutes />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route
+          path="*"
+          element={
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+                fontSize: "32px",
+                fontWeight: "bold",
+              }}
+            >
+              404 - Page Not Found
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
