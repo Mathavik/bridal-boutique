9c2380b395739cc06a30f2814f38ff8b50e83a56
Mathavik <mathavikanagaraj1@gmail.com>
2026-06-30 12:02:33 +0530

    first commit

--- Bridal-Boutique-Frontend/start-app.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting StartupConnect application...
-echo.
-echo This will open your browser to http://localhost:3000
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-cmd /c "npm start"
-pause 
